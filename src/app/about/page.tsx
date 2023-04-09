ffb83c9328cee8f177eefc31e403609b54f405cb
Francisco Moretti <franciscoemoretti@gmail.com>
2023-04-09 08:17:10 +0100

    getting content from file for index and about

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function AboutPage() {
+    return (
+      <section>
+        <h1 className="font-bold text-3xl font-serif">About Me</h1>
+        <p className="my-5 text-neutral-800 dark:text-neutral-200">
+          This is my about page.
+        </p>
+      </section>
+    );
+  }
---
+++ src/utils/info.tsx
@@ -0,0 +1,18 @@
+import me from '~/app/placeholder.jpg';
+
+export const name = 'Your Name';
+export const avatar = me;
+export const about = () => {
+  return (
+    <>
+      Hey, I'm a developer who loves Next.js. I work at BigCo or CoolStartup as a Software Engineer.
+    </>
+  );
+};
+export const bio = () => {
+  return (
+    <>
+      When I'm not coding, I'm probably playing video games or watching anime. I also love to travel and explore new places.
+    </>
+  );
+};
