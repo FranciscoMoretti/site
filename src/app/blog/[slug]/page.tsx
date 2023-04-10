02935edd9757b3a9ed0d0e8be486823f3f28d9e0
Francisco Moretti <franciscoemoretti@gmail.com>
2023-04-10 14:37:50 +0100

    remove dark mode from page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -70,10 +70,10 @@ export default async function Blog({ params }) {
         <Balancer>{post.title}</Balancer>
       </h1>
       <div className="grid grid-cols-[auto_1fr_auto] items-center mt-4 mb-8 font-mono text-sm max-w-[650px]">
-        <div className="bg-neutral-100 dark:bg-neutral-800 rounded-md px-2 py-1 tracking-tighter">
+        <div className="bg-neutral-100 rounded-md px-2 py-1 tracking-tighter">
           {post.publishedAt}
         </div>
-        <div className="h-[0.2em] bg-neutral-50 dark:bg-neutral-800 mx-2" />
+        <div className="h-[0.2em] bg-neutral-50  mx-2" />
         {/* <ViewCounter slug={post.slug} trackView /> */}
       </div>
       <Mdx code={post.body.code} />
