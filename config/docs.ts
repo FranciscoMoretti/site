090a68e34de9083ad8b2c37f73874fb6b485ed25
Francisco Moretti <franciscoemoretti@gmail.com>
2023-04-14 10:20:44 +0100

    autogenerate code-tips docs config

--- config/docs.ts
+++ config/docs.ts
@@ -1,136 +1,65 @@
 import { DocsConfig } from "types"
+import { allDocs } from "contentlayer/generated"
 
-export const docsConfig: DocsConfig = {
-  mainNav: [
-    {
-      title: "Documentation",
-      href: "/docs",
-    },
-    {
-      title: "Guides",
-      href: "/guides",
-    },
-  ],
-  sidebarNav: [
-    {
-      title: "Getting Started",
-      items: [
-        {
-          title: "Introduction",
-          href: "/docs",
-        },
-      ],
-    },
-    {
-      title: "Documentation",
-      items: [
-        {
-          title: "Introduction",
-          href: "/docs/documentation",
-        },
-        {
-          title: "Contentlayer",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Components",
-          href: "/docs/documentation/components",
-        },
-        {
-          title: "Code Blocks",
-          href: "/docs/documentation/code-blocks",
-        },
-        {
-          title: "Style Guide",
-          href: "/docs/documentation/style-guide",
-        },
-        {
-          title: "Search",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-      ],
-    },
-    {
-      title: "Blog",
-      items: [
-        {
-          title: "Introduction",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Build your own",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Writing Posts",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-      ],
-    },
-    {
-      title: "Dashboard",
-      items: [
-        {
-          title: "Introduction",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Layouts",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Server Components",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Authentication",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Database with Prisma",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "API Routes",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-      ],
-    },
-    {
-      title: "Marketing Site",
-      items: [
-        {
-          title: "Introduction",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "File Structure",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Tailwind CSS",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Typography",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-      ],
-    },
-  ],
+const simplifiedDocs = allDocs.map(
+  doc => (({
+    title: doc.title,
+    slug: doc.slug ? doc.slug : 'error_slug',
+    category: doc.category,
+    sidebar_position: doc.sidebar_position ? doc.sidebar_position : 0
+  })
+))
+
+interface DocPage {
+  title: string;
+  slug: string;
+  category: string;
+  sidebar_position: number;
 }
+
+function transformDocPagesToConfig(docPages: DocPage[]): DocsConfig {
+  const sidebarNav: DocsConfig["sidebarNav"] = [];
+
+  // Sort pages by sidebar_position
+  docPages.sort((a, b) => {
+    return a.sidebar_position - b.sidebar_position;
+  });
+
+  // group doc pages by category
+  const pagesByCategory = docPages.reduce((acc, page) => {
+    if (!acc[page.category]) {
+      acc[page.category] = [];
+    }
+    acc[page.category].push(page);
+    return acc;
+  }, {} as Record<string, DocPage[]>);
+
+  // build sidebarNav from grouped pages
+  Object.entries(pagesByCategory).forEach(([category, pages]) => {
+    const sidebarSection = {
+      title: category,
+      items: pages.map((page) => ({
+        title: page.title,
+        href: page.slug,
+      })),
+    };
+    sidebarNav.push(sidebarSection);
+  });
+
+  return {
+    mainNav: [
+      {
+        title: "Documentation",
+        href: "/docs",
+      },
+      {
+        title: "Guides",
+        href: "/guides",
+      },
+    ],
+    sidebarNav,
+  };
+}
+
+const codeTipsDocs = simplifiedDocs.filter(doc => doc.slug.startsWith('/docs/code-tips'))
+export const docsConfig = transformDocPagesToConfig(codeTipsDocs);
