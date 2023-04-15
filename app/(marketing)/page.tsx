6a067778b450fbf5644a2ab2715c8b6fc44dbd99
Francisco Moretti <franciscoemoretti@gmail.com>
2023-04-15 17:37:49 +0100

    changed hero content

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -42,11 +42,11 @@ export default async function IndexPage() {
         <Image src={hero} width={250} alt="Hero image" priority />
         <div className="mx-auto flex flex-col items-start gap-4 lg:w-[52rem]">
           <h1 className="text-3xl font-bold leading-[1.1] tracking-tighter sm:text-5xl md:text-6xl">
-            What&apos;s going on here?
+            Francisco Moretti
           </h1>
           <p className="max-w-[42rem] leading-normal text-slate-700 sm:text-xl sm:leading-8">
-            I&apos;m building a web app with Next.js 13 and open sourcing
-            everything. Follow along as we figure this out together.
+            Software Development Engineering at iRobot | Web, Design, Best
+            Practices
           </p>
         </div>
         <div className="flex gap-4">
