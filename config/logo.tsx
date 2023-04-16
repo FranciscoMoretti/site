c2ec9de7a7664d6c8bfa87af85fd439a053e29a4
Francisco Moretti <franciscoemoretti@gmail.com>
2023-04-16 10:02:28 +0100

    fix: make classname optional

--- config/logo.tsx
+++ config/logo.tsx
@@ -1,13 +1,10 @@
-import logo from '@/public/favicon.svg'
-import Image from 'next/image';
+import logo from "@/public/favicon.svg"
+import Image from "next/image"
 
-export function Logo({className= 'h-6 w-6'}: {className: string }) {
-    return (
-        <div className={className}>
-            <Image
-                src={logo}
-                alt='Logo'
-            />
-        </div>
-    );
-  }
+export function Logo({ className = "h-6 w-6" }: { className?: string }) {
+  return (
+    <div className={className}>
+      <Image src={logo} alt="Logo" />
+    </div>
+  )
+}
