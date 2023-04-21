ae47f536abee11856bacff85dd6a12f1a3def2af
Francisco Moretti <franciscoemoretti@gmail.com>
2023-04-21 20:04:13 +0100

    next config from mjs to js to hide warns

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-import { withContentlayer } from "next-contentlayer"
+const { withContentlayer } = require("next-contentlayer")
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -12,4 +12,4 @@ const nextConfig = {
   },
 }
 
-export default withContentlayer(nextConfig)
+module.exports = withContentlayer(nextConfig)
