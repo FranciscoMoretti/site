c1a4e588a6efe23b6ef12bac70b560aeb79ffd1d
Francisco Moretti <franciscoemoretti@gmail.com>
2023-05-01 07:52:37 +0000

    Update sitemap.ts to avoid redirects

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,18 +2,18 @@ import { allPosts, allDocs } from "contentlayer/generated"
 
 export default async function sitemap() {
   const posts = allPosts.map((post) => ({
-    url: `https://franciscomoretti.com${post.slug}`,
+    url: `https://www.franciscomoretti.com${post.slug}`,
     lastModified: post.date,
   }))
 
   const docs = allDocs.map((doc) => ({
-    url: `https://franciscomoretti.com${doc.slug}`,
+    url: `https://www.franciscomoretti.com${doc.slug}`,
     lastModified: doc.date,
   }))
 
   // const routes = ["", "/about", "/blog", "/guestbook", "/uses"].map(
   const routes = ["", "/blog"].map((route) => ({
-    url: `https://franciscomoretti.com${route}`,
+    url: `https://www.franciscomoretti.com${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }))
 
