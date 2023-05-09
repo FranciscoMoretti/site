137676ae61316ce38618e88126268c02a02a5377
Francisco Moretti <franciscoemoretti@gmail.com>
2023-05-09 07:17:27 +0100

    added more redirects

--- next.config.js
+++ next.config.js
@@ -13,6 +13,11 @@ const nextConfig = {
   async redirects() {
     return [
       {
+        source: "/blog-posts",
+        destination: "/blog",
+        permanent: true,
+      },
+      {
         source: "/use-a-lite-youtube-embedded-player-in-nextjs",
         destination: "/blog/use-a-lite-youtube-embedded-player-in-nextjs",
         permanent: true,
@@ -42,6 +47,93 @@ const nextConfig = {
       {
         source: "/zero-to-nextjs-example-with-wsl2-in-w11",
         destination: "/blog/zero-to-nextjs-example-with-wsl2-in-w11",
+        permanent: true,
+      },
+      {
+        source: "/next-conf-2021-notes-and-resources",
+        destination: "/blog/next-conf-2021-notes-and-resources",
+        permanent: true,
+      },
+      {
+        source: "/composition-vs-inheritance-which-is-better-for-your-code",
+        destination:
+          "/code-tips/composition-vs-inheritance-which-is-better-for-your-code",
+        permanent: true,
+      },
+      {
+        source: "/composition-vs-inheritance-which-is-better-for-your-code",
+        destination:
+          "/code-tips/composition-vs-inheritance-which-is-better-for-your-code",
+        permanent: true,
+      },
+      {
+        source: "/dont-add-unneeded-context",
+        destination: "/code-tips/dont-add-unneeded-context",
+        permanent: true,
+      },
+      {
+        source: "/dont-over-optimize",
+        destination: "/code-tips/dont-over-optimize",
+        permanent: true,
+      },
+      {
+        source: "/dont-use-flags-as-function-parameters",
+        destination: "/code-tips/dont-use-flags-as-function-parameters",
+        permanent: true,
+      },
+      {
+        source: "/dont-use-magic-numbers",
+        destination: "/code-tips/dont-use-magic-numbers",
+        permanent: true,
+      },
+      {
+        source: "/avoid-mental-mapping",
+        destination: "/code-tips/avoid-mental-mapping",
+        permanent: true,
+      },
+      {
+        source: "/few-function-arguments",
+        destination: "/code-tips/few-function-arguments",
+        permanent: true,
+      },
+      {
+        source: "/function-names-should-say-what-they-do",
+        destination: "/code-tips/function-names-should-say-what-they-do",
+        permanent: true,
+      },
+      {
+        source: "/prefer-es6-classes-over-es5-functions",
+        destination: "/code-tips/prefer-es6-classes-over-es5-functions",
+        permanent: true,
+      },
+      {
+        source: "/functions-should-do-one-thing",
+        destination: "/code-tips/functions-should-do-one-thing",
+        permanent: true,
+      },
+      {
+        source: "/interface-segregation-principle-isp--solid",
+        destination: "/code-tips/interface-segregation-principle-isp--solid",
+        permanent: true,
+      },
+      {
+        source: "/kiss-keep-it-short-and-simple",
+        destination: "/code-tips/kiss-keep-it-short-and-simple",
+        permanent: true,
+      },
+      {
+        source: "/liskov-substitution-principle-lsp--solid",
+        destination: "/code-tips/liskov-substitution-principle-lsp--solid",
+        permanent: true,
+      },
+      {
+        source: "/open-closed-principle-ocp--solid",
+        destination: "/code-tips/open-closed-principle-ocp--solid",
+        permanent: true,
+      },
+      {
+        source: "/avoid-negative-conditionals",
+        destination: "/code-tips/avoid-negative-conditionals",
         permanent: true,
       },
       {
