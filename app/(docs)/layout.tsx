bb61fcf7e8a5c75ff2ad223d20602a28592b581c
Francisco Moretti <franciscoemoretti@gmail.com>
2023-06-01 11:09:22 +0100

    add styling to separators

--- app/(docs)/layout.tsx
+++ app/(docs)/layout.tsx
@@ -15,7 +15,7 @@ interface DocsLayoutProps {
 export default function DocsLayout({ children }: DocsLayoutProps) {
   return (
     <div className="flex min-h-screen flex-col">
-      <header className="sticky top-0 z-40 w-full border-b border-b-slate-200 bg-white">
+      <header className="sticky top-0 z-40 w-full border-b border-b-secondary bg-white">
         <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
           <MainNav items={docsConfig.mainNav}>
             <DocsSidebarNav items={docsConfig.sidebarNav} />
