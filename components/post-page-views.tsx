a1a2368d1a4ddba6320691fb8db8576edf74b0c6
Francisco Moretti <franciscoemoretti@gmail.com>
2023-06-12 12:23:31 +0100

    only get views in postpageviews comp

--- components/post-page-views.tsx
+++ components/post-page-views.tsx
@@ -1,10 +1,6 @@
-import { updatePostViews } from "@/app/(marketing)/actions"
 import { getPostViews } from "@/app/(marketing)/actions"
 
 export async function PostPageViews({ slug }: { slug: string }) {
-  const count =
-    process.env.NODE_ENV === "production"
-      ? await updatePostViews({ slug })
-      : await getPostViews({ slug })
+  const count = await getPostViews({ slug })
   return <span title="Views">{count} views</span>
 }
