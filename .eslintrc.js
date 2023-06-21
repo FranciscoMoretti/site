4da169d2329ab9935d1aa2da76011676aff8b5c3
Francisco Moretti <franciscoemoretti@gmail.com>
2023-06-21 10:46:11 +0100

    added typescript eslint package

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     "prettier",
     "plugin:tailwindcss/recommended",
   ],
-  plugins: ["tailwindcss"],
+  plugins: ["@typescript-eslint", "tailwindcss"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
