94c389e11eeb8b115678aa6824d95fe60e01f24c
Francisco Moretti <franciscoemoretti@gmail.com>
2023-06-28 19:21:56 +0100

    adjust title size a bit

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -77,7 +77,7 @@ export default async function IndexPage() {
       </section>
       <hr className="container border-secondary" />
       <section className="container space-y-8 py-8 md:max-w-4xl md:py-12 lg:py-16">
-        <h2 className="mb-4 scroll-m-20 pb-1 text-2xl font-semibold tracking-tight first:mt-0 md:text-3xl">
+        <h2 className="mb-4 scroll-m-20 pb-1 text-2xl font-semibold tracking-tight first:mt-0 md:text-5xl">
           {"Recent Posts"}
         </h2>
         <BlogPostList posts={posts} />
