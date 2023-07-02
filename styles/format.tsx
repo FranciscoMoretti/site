b3cac483e5f7733f2077af47424538c66bfe85d9
Francisco Moretti <franciscoemoretti@gmail.com>
2023-07-02 15:33:32 +0100

    some space in format styles

--- styles/format.tsx
+++ styles/format.tsx
@@ -1,16 +1,30 @@
 export const FORMAT_H1 = "mt-2 scroll-m-20 text-4xl font-bold tracking-tight"
+
 export const FORMAT_H2 =
   "mt-10 scroll-m-20 border-b border-b-slate-200 pb-1 text-3xl font-semibold tracking-tight first:mt-0"
+
 export const FORMAT_H3 =
   "mt-8 scroll-m-20 text-2xl font-semibold tracking-tight"
+
 export const FORMAT_H4 = "mt-8 scroll-m-20 text-xl font-semibold tracking-tight"
+
 export const FORMAT_H5 = "mt-8 scroll-m-20 text-lg font-semibold tracking-tight"
+
 export const FORMAT_H6 =
   "mt-8 scroll-m-20 text-base font-semibold tracking-tight"
+
 export const FORMAT_A =
   "font-medium text-slate-900 underline underline-offset-4"
+
 export const FORMAT_P = "leading-7 [&:not(:first-child)]:mt-6"
+
 export const FORMAT_UL = "my-6 ml-6 list-disc"
+
 export const FORMAT_OL = "my-6 ml-6 list-decimal"
+
 export const FORMAT_LI = "mt-2"
+
 export const FORMAT_HR = "my-4 border-slate-200 md:my-8"
+
+export const FORMAT_LINKED_HEADER =
+  "before:content-['#'] before:absolute before:invisible before:-ml-[1em] hover:before:visible before:text-slate-300 pl-[1em] -ml-[1em]"
