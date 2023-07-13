54ecd79addbc497f8983456007c9931ba289cd4f
Francisco Moretti <franciscoemoretti@gmail.com>
2023-07-13 20:51:20 +0100

    update bg color and toggle

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -13,20 +13,14 @@ import {
 import { Icons } from "@/components/icons"
 
 export function ModeToggle() {
-  const { theme, setTheme } = useTheme()
-  console.log(theme)
+  const { setTheme } = useTheme()
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="m-2 w-9 px-2">
-          {theme == "dark" ? (
-            <Icons.moon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all" />
-          ) : theme == "light" ? (
-            <Icons.sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all " />
-          ) : (
-            <Icons.laptop className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
-          )}
+        <Button variant="ghost" className="w-9 px-0">
+          <Icons.sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Icons.moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
