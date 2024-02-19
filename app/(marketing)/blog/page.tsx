243f891fa9f49f96b9799f44ff7100117ec89656
Francisco Moretti <franciscoemoretti@gmail.com>
2024-02-19 09:21:47 +0000

    fix alignment

--- app/(marketing)/blog/page.tsx
+++ app/(marketing)/blog/page.tsx
@@ -20,8 +20,8 @@ export default async function BlogPage() {
 
   return (
     <div className="container max-w-4xl py-6 lg:py-10">
-      <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
-        <div className="flex-1 space-y-4">
+      <div className="flex flex-col items-start gap-4 md:gap-8">
+        <div className="flex flex-1 flex-col space-y-4">
           <h1 className="inline-block text-4xl font-extrabold tracking-tight lg:text-5xl">
             The Happy Coding Blog!
           </h1>
