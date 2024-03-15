8c5a51b4877aa288d933fe2ecf15fe38407e5a52
Francisco Moretti <franciscoemoretti@gmail.com>
2024-03-15 18:58:09 +0000

    fix build

--- app/api/thumbnail/route.tsx
+++ app/api/thumbnail/route.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { ImageResponse } from "@vercel/og"
+import { ImageResponse } from "next/og"
 
 import { thumbnailImageSchema } from "@/lib/validations/thumbnail"
 
