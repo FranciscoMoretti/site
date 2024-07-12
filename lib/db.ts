ac6fd9a3fc01daa7c00b90e0f3a6f5e116d0f038
Francisco Moretti <franciscoemoretti@gmail.com>
2024-07-12 13:17:50 +0100

    don't init db if disabled

--- lib/db.ts
+++ lib/db.ts
@@ -1,13 +1,20 @@
+import { env } from "process"
 import { createClient } from "@libsql/client"
 import { PrismaLibSQL } from "@prisma/adapter-libsql"
 import { PrismaClient } from "@prisma/client"
 
-const libsql = createClient({
-  url: `${process.env.TURSO_DATABASE_URL}`,
-  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
-})
+function createPrismaClient() {
+  if (process.env.DISABLE_DB) {
+    return null
+  }
 
-const adapter = new PrismaLibSQL(libsql)
-const prisma = new PrismaClient({ adapter })
+  const libsql = createClient({
+    url: `${process.env.TURSO_DATABASE_URL}`,
+    authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+  })
 
-export const db = prisma
+  const adapter = new PrismaLibSQL(libsql)
+  const prisma = new PrismaClient({ adapter })
+  return prisma
+}
+export const db = createPrismaClient()
