5dfeac82859dbd1c556c612d507269d6a78f394e
Francisco Moretti <franciscoemoretti@gmail.com>
2024-12-31 09:33:26 +0000

    test reduce home loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,19 @@
 import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
 import { Home } from './Home'
-import { getAllViews } from './actions'
+// import { getAllViews } from './actions'
 
 export default async function Page() {
   const sortedPosts = sortPosts(allBlogs.filter((blog) => blog.draft !== true))
   const posts = allCoreContent(sortedPosts)
+  return <Home posts={posts} />
 
-  const views = await getAllViews()
-  const viewsBySlug =
-    views?.reduce((acc, view) => {
-      acc[view.slug] = view.views
-      return acc
-    }, {}) || {}
+  // const views = await getAllViews()
+  // const viewsBySlug =
+  //   views?.reduce((acc, view) => {
+  //     acc[view.slug] = view.views
+  //     return acc
+  //   }, {}) || {}
 
-  return <Home posts={posts.map((post) => ({ ...post, viewCount: viewsBySlug[post.slug] }))} />
+  // return <Home posts={posts.map((post) => ({ ...post, viewCount: viewsBySlug[post.slug] }))} />
 }
