ca2d2020ea70b3936937af0bc263139d27030256
Francisco Moretti <franciscoemoretti@gmail.com>
2025-01-02 18:02:27 +0000

    bye zod

--- lib/validations/og.ts
+++ lib/validations/og.ts
@@ -1,9 +1,5 @@
-import * as z from 'zod'
-
-export const ogImageSchema = z.object({
-  heading: z.string(),
-  type: z.string(),
-  mode: z.enum(['light', 'dark']).optional().default('dark'),
-})
-
-export type OgImageSchema = z.infer<typeof ogImageSchema>
+export type OgImageSchema = {
+  heading: string
+  type: string
+  mode: 'light' | 'dark'
+}
