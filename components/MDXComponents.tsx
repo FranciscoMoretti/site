eb705d419f31622eb428d79c414cad24af658563
Francisco Moretti <francisco@samaya.ai>
2025-04-24 10:38:36 +0100

    image dialog

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -6,12 +6,22 @@ import CustomLink from './Link'
 import TableWrapper from './TableWrapper'
 import { CopyButton } from './copy-button'
 import { cn } from './lib/utils'
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
 
 export const components: MDXComponents = {
-  Image: ({ ...rest }) => (
-    <div className="m-0 w-full rounded-xl bg-muted/80 p-2">
-      <Image className="m-0 w-full rounded-lg" {...rest} />
-    </div>
+  Image: ({ alt, src, ...rest }) => (
+    <Dialog>
+      <DialogTrigger asChild>
+        <div className="m-0 w-full cursor-pointer rounded-xl bg-muted/80 p-2">
+          <Image className="m-0 w-full rounded-lg" alt={alt || ''} src={src} {...rest} />
+        </div>
+      </DialogTrigger>
+      <DialogContent className="bg-card sm:max-w-[80vw]">
+        <div className="m-0 w-full rounded-xl bg-card p-2">
+          <Image className="w-full rounded-lg object-contain" alt={alt || ''} src={src} {...rest} />
+        </div>
+      </DialogContent>
+    </Dialog>
   ),
   TOCInline,
   a: CustomLink,
